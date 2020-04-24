aaa9265cceee19d7efa9b65b8c1f60de57ba6992
futaichi <64215412+futaichi@users.noreply.github.com>
2020-04-24 20:10:42 +0900

    sample3

--- discordbot.py
+++ discordbot.py
@@ -11,18 +11,6 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-    
-@client.event
-async def on_message(message):
-    if message.content.startswith("はろー"):
-        m = "こんにちは、" + message.author.name + "さん"
-        await client.send_message(message.channel,m)
 
 
 @bot.command()
