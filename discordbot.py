c22642234c2f582bb1ab67fb8bd1935550f4195b
futaichi <64215412+futaichi@users.noreply.github.com>
2020-04-25 17:21:05 +0900

    sample11

--- discordbot.py
+++ discordbot.py
@@ -1,23 +1,23 @@
-from discord.ext import commands
-import os
-import traceback
 
-bot = commands.Bot(command_prefix='/')
+
+
+import discord
+import asyncio
+
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
+@client.event
+async def on_message(message):
+    if message.content.startswith("はろー"):
+        m = "こんにちは、" + message.author.name + "さん"
+        await client.send_message(message.channel,m)
 
-@bot.command()
-async def suga(ctx):
-    
-        m = "こんにちは、" + command.author.name + "さん"
-        await ctx.send(m)
-    #await ctx.send('がぬまち')
-    
-
-bot.run(token)
+client.run('token')
