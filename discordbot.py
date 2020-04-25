c41aa0f0d4f6e444a4798262eebdd823889b0e71
futaichi <64215412+futaichi@users.noreply.github.com>
2020-04-25 18:35:05 +0900

    sample12

--- discordbot.py
+++ discordbot.py
@@ -7,12 +7,7 @@ import asyncio
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+
 
 @client.event
 async def on_message(message):
